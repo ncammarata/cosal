1400a6c770e70f115f93495e87bc71788c4a9b4a
Nick Cammarata <cammarata.nick@gmail.com>
2018-04-04 16:14:59 -0700

    first commit

--- test.js
+++
@@ -1,6 +0,0 @@
-const { matrix, inv } = require("mathjs")
-const { range } = require("lodash")
-const cov = require("compute-covariance")
-
-x = range(10).map(x => range(100).map(y => Math.random()))
-console.log(inv(matrix(cov(x))))
